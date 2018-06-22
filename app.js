92b57bdb86a39b9bfbbbd5d9b218896334182c42
David Hollema <david.hollema@edf-re.com>
2018-06-22 16:06:26 -0600

    Fix projects endpoins

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/projects', function(req, res) {
-  res.send({
-    "got all projects"
-  });
+  res.send({"message":"got all projects"});
 });
 
 app.post('/projects', function(req, res) {
-  res.send({
-    "added new project"
-  });
+  res.send({"message":"added new project"});
 });
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
--- tests/test.js
+++ tests/test.js
@@ -7,16 +7,16 @@ const app = require('../app.js');
 const request = supertest(app);
 
 describe('Tests app', function() {
-  it('verifies get', function(done) {
-    request.get('/').expect(200).end(function(err, result) {
-        test.string(result.body.Output).contains('Hello');
+  it('verifies get all projects', function(done) {
+    request.get('/projects').expect(200).end(function(err, result) {
+        test.string(result.body.message).contains('project');
         test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
         done(err);
     });
   });
-  it('verifies post', function(done) {
-    request.post('/').expect(200).end(function(err, result) {
-        test.string(result.body.Output).contains('Hello');
+  it('verifies post project', function(done) {
+    request.post('/projects').expect(200).end(function(err, result) {
+        test.string(result.body.message).contains('project');
         test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
         done(err);
     });
