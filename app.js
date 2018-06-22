648bd88741f4e61b25b5a32fc97048d63bcf79cb
David Hollema <david.hollema@edf-re.com>
2018-06-22 16:20:48 -0600

    Add remaining endpoints

--- app.js
+++ app.js
@@ -9,5 +9,17 @@ app.post('/projects', function(req, res) {
   res.send({"message":"added new project"});
 });
 
+app.get('/project/{projectId}', function(req, res) {
+  res.send({"message":"got project"});
+});
+
+app.put('/project/{projectId}', function(req, res) {
+  res.send({"message":"updated project"});
+});
+
+app.delete('/project/{projectId}', function(req, res) {
+  res.send({"message":"deleted project"});
+});
+
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
